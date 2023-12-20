62b3418479a7f7e74fc8ddbc7ffcae5111c589ad
Aditya Gupta <gaditya060@gmail.com>
2023-12-20 20:42:45 +0530

    Add New Albums Grid

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,17 +1,57 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from './components/Hero/Hero';
 import Section from "./components/Section/Section";
+import { useState, useEffect } from  "react";
+import axios from "axios";
+import "./App.css";
 
 export const config = {
-  endpoint: "https://qtify-backend-labs.crio.do/albums/top"
+  endpoint: "https://qtify-backend-labs.crio.do/albums"
 };
 
 function App() {
+  const [topAlbumsList, setTopAlbumsList] = useState([]);
+  const [newAlbumsList, setNewAlbumsList] = useState([]);
+
+  const getTopAlbums = async () => {
+    try {
+      const apiResponse = await axios.get(config.endpoint+"/top");
+      return apiResponse.data;
+    }
+    catch (e) {
+      console.error(e);
+    }
+  }
+
+  const getNewAlbums = async () => {
+    try {
+      const apiResponse = await axios.get(config.endpoint+"/new");
+      return apiResponse.data;
+    }
+    catch (e) {
+      console.error(e);
+    }
+  }
+
+  useEffect(() => {
+    const onLoadHandler = async () => {
+      const topAlbumsData = await getTopAlbums();
+      const newAlbumsData = await getNewAlbums();
+      setTopAlbumsList(topAlbumsData);
+      setNewAlbumsList(newAlbumsData);
+    }
+    onLoadHandler();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <Hero />
-      <Section />
+      <main className='section-container'>
+        <Section topAlbums={topAlbumsList} />
+        <div className='ruler'></div>
+        <Section newAlbums={newAlbumsList} />
+      </main>
     </div>
   );
 }
--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -1,44 +1,20 @@
 import styles from "./Section.module.css";
-import { useState, useEffect } from "react";
-import { config } from "../../App";
 import Card from "../Card/Card";
-import axios from "axios";
 
-function Section(props) {
-  const [topAlbumsData, setTopAlbumsData] = useState([]);
-
-  const getTopAlbums = async () => {
-    try {
-      const apiResponse = await axios.get(config.endpoint);
-      return apiResponse.data;
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  useEffect(() => {
-    const onLoadHandler = async () => {
-      const albumsData = await getTopAlbums();
-      setTopAlbumsData(albumsData);
-    };
-    onLoadHandler();
-  }, []);
-
+function Section({topAlbums=[], newAlbums=[]}) {
   return (
     <div className={styles.section}>
-      <div className={styles["top-album-section"]}>
-        <div className={styles["top-album-heading"]}>
-          <p>Top Albums</p>
+      <div className={styles["album-section"]}>
+        <div className={styles["album-heading"]}>
+          <p>{topAlbums.length > 0 ? 'Top Albums' : 'New Albums'}</p>
           <button className={styles["btn-show-collapse"]}>Collapse</button>
         </div>
-        <div className={styles["top-album-grid"]}>
-          {topAlbumsData.map((album) => (
-            <Card
-              image={album.image}
-              follows={album.follows}
-              title={album.title}
-            />
-          ))}
+        <div className={styles["album-grid"]}>
+          {
+            topAlbums.length > 0 ? 
+            topAlbums.map((album) => <Card image={album.image} follows={album.follows} title={album.title} />) : 
+            newAlbums.map((album) => <Card image={album.image} follows={album.follows} title={album.title} />)
+            }
         </div>
       </div>
     </div>
