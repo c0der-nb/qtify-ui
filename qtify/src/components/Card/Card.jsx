258a33e76225b7c561f057be18ed9ca39f78e71d
Aditya Gupta <gaditya060@gmail.com>
2024-01-04 22:37:27 +0530

    Add tooltip for album card

--- qtify/src/components/Card/Card.jsx
+++ qtify/src/components/Card/Card.jsx
@@ -1,26 +1,29 @@
 import styles from "./Card.module.css";
 import Chip from "@mui/material/Chip";
+import Tooltip from '@mui/material/Tooltip';
 
 function Card({ data, type }) {
   if (type === 'album') {
     return (
-      <div className={styles.card}>
-        <div className={styles["card-img-pill"]}>
-          <img src={data.image} className={styles["card-image"]} alt={data.title} />
-          <div className={styles.banner}>
-            <Chip
-              sx={{
-                backgroundColor: "var(--color-black)",
-                color: "var(--color-white)",
-                fontSize: "10px",
-              }}
-              size="small"
-              label={data.follows + " Follows"}
-            />
+      <Tooltip title={data.songs.length+' songs'} placement="top">
+        <div className={styles.card}>
+          <div className={styles["card-img-pill"]}>
+            <img src={data.image} className={styles["card-image"]} alt={data.title} />
+            <div className={styles.banner}>
+              <Chip
+                sx={{
+                  backgroundColor: "var(--color-black)",
+                  color: "var(--color-white)",
+                  fontSize: "10px",
+                }}
+                size="small"
+                label={data.follows + " Follows"}
+              />
+            </div>
           </div>
+          <p>{data.title}</p>
         </div>
-        <p>{data.title}</p>
-      </div>
+      </Tooltip>
     );
   }
   else if (type === 'song') {
